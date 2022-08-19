29769b9c542505049b401b66584836fff1007837
Jeffry Gonzalez <jeff@hypertheory.com>
2022-08-19 09:32:10 -0400

    consumer

---
+++ Consumers/ConsumeDocumentProtobufBackgroundService.cs
@@ -0,0 +1,102 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Google.Protobuf;
+using Microsoft.Extensions.Hosting;
+
+namespace Hypertheory.KafkaUtils.Consumers;
+
+
+
+/// <summary>
+/// Based on https://github.com/confluentinc/confluent-kafka-dotnet/blob/master/examples/Web/RequestTimeConsumer.cs
+/// </summary>
+/// <typeparam name="TKey">The type of the incoming key</typeparam>
+/// <typeparam name="TValue">The type of the incoming message</typeparam>
+public abstract class ConsumeDocumentProtobufBackgroundService<TKey, TValue> : BackgroundService
+    where TValue: class, IMessage<TValue>, new()
+    where TKey: class, IMessage<TKey>, new()
+
+{
+    private readonly string _topic;
+
+    private readonly IConsumer<TKey, TValue> _kafkaConsumer;
+
+
+
+
+
+    public ConsumeDocumentProtobufBackgroundService(ConsumerConfig consumerConfig, string topic)
+    {
+       
+        _topic = topic;
+        _kafkaConsumer = new ConsumerBuilder<TKey, TValue>(consumerConfig)
+            .SetValueDeserializer(new ProtobufDeserializer<TValue>
+                ().AsSyncOverAsync())
+            .SetKeyDeserializer(new ProtobufDeserializer<TKey>().AsSyncOverAsync())
+
+            .Build();
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        new Thread(() => StartConsumerLoop(stoppingToken)).Start();
+
+        return Task.CompletedTask;
+    }
+
+    private void StartConsumerLoop(CancellationToken cancellationToken)
+    {
+        _kafkaConsumer.Subscribe(this._topic);
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var cr = this._kafkaConsumer.Consume(cancellationToken);
+
+                // Handle message...
+                HandleConsumeLoop(cr);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (ConsumeException e)
+            {
+                // Consumer errors should generally be ignored (or logged) unless fatal.
+                HandleConsumeException(e);
+
+                if (e.Error.IsFatal)
+                {
+                    // https://github.com/edenhill/librdkafka/blob/master/INTRODUCTION.md#fatal-consumer-errors
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e}");
+                break;
+            }
+        }
+    }
+
+    protected virtual void HandleConsumeException(ConsumeException e)
+    {
+        Console.WriteLine($"ConsumeException {e.Error.Reason}");
+    }
+
+
+
+    protected abstract void HandleConsumeLoop(ConsumeResult<TKey, TValue> result);
+
+
+    public override void Dispose()
+    {
+        this._kafkaConsumer.Close(); // Commit offsets and leave the group cleanly.
+        this._kafkaConsumer.Dispose();
+
+        base.Dispose();
+    }
+}
